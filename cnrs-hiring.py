abac4f71abd88d526f8f24987f3824352fbafe08
Martin Clavey <martin.clavey@gmail.com>
2017-01-10 16:39:12 +0100

    prise en compte des changements des sections en 2013

--- cnrs-hiring.py
+++ cnrs-hiring.py
@@ -12,7 +12,7 @@ class arreteCR:
         self.year=1970
         self.classe=""
         self.url=url
-        self.postes={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0,26:0,27:0,28:0,29:0,30:0,31:0,32:0,33:0,34:0,35:0,36:0,37:0,38:0,39:0,40:0,41:0,42:0,43:0,44:0,45:0,46:0,47:0,48:0,51:0,52:0,53:0,54:0}
+        self.postes={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,11:0,12:0,13:0,14:0,15:0,16:0,17:0,18:0,19:0,20:0,21:0,22:0,23:0,24:0,25:0,26:0,27:0,28:0,29:0,30:0,31:0,32:0,33:0,34:0,35:0,36:0,37:0,38:0,39:0,40:0,41:0,"Interdisciplinaires":0}
         
     def __str__(self):
         return  "Arrêté postes CNRS Année %s, classe %s" % (self.year, self.classe)
@@ -30,6 +30,10 @@ class arreteCR:
         if titleData is not None:
             self.year=int(titleData.group('datePubli'))
             self.classe=titleData.group('classe')
+            
+        # correspondance sections avant 2013 et après 2013
+        correspondances={1:41,3:1,6:3,7:[6,7],20:30,21:20,22:21,23:22,24:[24,25,27],25:24,26:22,27:26,28:23,30:28}
+
         #Récupération du document coupé selon les sections
         sections=re.split("Section ",str(body),flags=re.S)
         total=0
@@ -39,11 +43,13 @@ class arreteCR:
             p = re.compile(expression)  
             sectionTable=re.findall(p,section.replace('\n',''))
             totalSection=0
+            
             if len(sectionTable)>0:
                 text=sectionTable[0][1]
+                
                 #On a coupé par section mais on a aussi des commissions interdisciplinaires. Récupérons dans les sections!
                 commissions=re.split("Commission interdisciplinaire",str(sectionTable[0]),flags=re.S)
-                #Si on a des commissions, il faut les intégrer au tableau
+                #Si on a des commissions, il faut les intégrer au tableau 
                 if len(commissions)>1:
                     for commission in commissions[1:]:
                         totalComm=0
@@ -60,16 +66,39 @@ class arreteCR:
                                 totalComm=totalComm+int(nbrPostes.group(1))
                             total=total+totalComm
                             #On attribue à l'indice du tableau correspondant au numéro de commission le nombre de postes attribués
-                            self.postes[int(numComm)]=totalComm
+                            self.postes["Interdisciplinaires"]=self.postes["Interdisciplinaires"]+totalComm
                     #On supprime de la section toutes les commissions        
                     text=commissions[0]                          
+                
                 #On compte le nombre de postes attribués à la Section    
                 postesTable=re.findall(r"N° *[0-9]+/[0-9]+[ .-]*[0-9]+", text)
                 for postes in postesTable:
                     nbrPostes=re.match(r"N° *[0-9]+/[0-9]+[ .-]*([0-9]+)",postes)
                     totalSection=totalSection+int(nbrPostes.group(1))
-                #On attribue à l'indice du tableau correspondant au numéro de commission le nombre de postes attribués    
-                self.postes[int(sectionTable[0][0])]=totalSection
+                numSection=int(sectionTable[0][0])
+                # On attribue à l'indice du tableau correspondant au numéro de section le nombre de postes attribués  
+                # En prenant compte du tableau des correspondances de sections avant 2013
+                
+                
+                if self.year<2013 and  (numSection in correspondances):
+                    
+                    #si les postes sont redistrubés dans plusieurs sections, on redistribue 
+                    if type(correspondances[numSection]).__name__ == 'list':
+                        #redistribution équiproportionnelle
+                        postescorresp=totalSection/len(correspondances[numSection])
+                        postescorresp = totalSection // len(correspondances[numSection])
+                        nbReste = totalSection % len(correspondances[numSection])
+                        for newSection in correspondances[numSection]:
+                            self.postes[newSection]=self.postes[newSection]+postescorresp
+                        #ajout du relicat de la redistribution à la dernière section
+                        self.postes[correspondances[numSection][-1]]=self.postes[correspondances[numSection][-1]]+nbReste
+                    # sinon on ajoute à la section correspondante
+                    else:
+                        self.postes[correspondances[numSection]]=self.postes[correspondances[numSection]]+totalSection
+                        
+                else:                      
+                    self.postes[numSection]=self.postes[numSection]+totalSection
+                    
             total=total+totalSection
         self.postes["total"]=total
         return self.postes
@@ -89,27 +118,31 @@ arretesCRSorted=sorted(arretesCR, key=attrgetter('year'))
 jsontab=[]
 jsonTabClasse1=[]
 jsonTabClasse2=[]
-sections=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,51,52,53,54]
-sectionsTitle=["Interactions, particules, noyaux, du laboratoire au cosmos","Théories physiques : méthodes, modèles et applications","Matière condensée : structures et propriétés électroniques","Atomes et molécules, optique et lasers, plasmas chauds","Matière condensée : organisation et dynamique","Sciences de l'information : fondements de l'informatique, calculs, algorithmes, représentations, exploitations","Sciences de l'information : signaux, images, langues, automatique, robotique, interactions, systèmes intégrés matériel-logiciel","Micro- et nanotechnologies, micro- et nanosystèmes, photonique, électronique, électromagnétisme, énergie électrique","Mécanique des solides. Matériaux et structures. Biomécanique. Acoustique","Milieux fluides et réactifs : transports, transferts, procédés de transformation","Systèmes et matériaux supra et macromoléculaires : élaboration, propriétés, fonctions","Architectures moléculaires : synthèses, mécanismes et propriétés","Chimie physique, théorique et analytique","Chimie de coordination, catalyse, interfaces et procédés","Chimie des matériaux, nanomatériaux et procédés","Chimie et vivant","Système solaire et univers lointain","Terre et planètes telluriques : structure, histoire, modèles","Système terre : enveloppes superficielles","Biologie moléculaire et structurale, biochimie","Organisation, expression, évolution des génomes. Bioinformatique et biologie des systèmes","Biologie cellulaire, développement, évolution-développement","Biologie végétale intégrative","Physiologie, vieillissement, tumorigenèse","Neurobiologie moléculaire et cellulaire, neurophysiologie","Cerveau, cognition, comportement","Relations hôte-pathogène, immunologie, inflammation","Pharmacologie-ingénierie et technologies pour la santé-imagerie biomédicale","Biodiversité, évolution et adaptations biologiques : des macromolécules aux communautés","Surface continentale et interfaces","Hommes et milieux : évolution, interactions","Mondes anciens et médiévaux","Mondes modernes et contemporains","Sciences du langage","Sciences philosophiques et philologiques, sciences de l'art","Sociologie et sciences du droit","Economie et gestion","Anthropologie et étude comparative des sociétés contemporaines","Espaces, territoires et sociétés","Politique, Pouvoir, Organisation","Mathématiques et interactions des mathématiques","Sciences de la communication","Modélisation des systèmes biologiques, bioinformatique","Cognition, langage,traitement de l'information, systèmes naturels et artificiels","Dynamique des systèmes environnementaux,développement durable, santé et société","Risques environnementaux et société","astroparticules","sciences de la communication","Modélisation et analyse des données et des systèmes biologiques : approches informatiques, mathématiques et physiques","Environnements sociétés : du fondamental à l'opérationnel","Méthodes, pratiques et communications des sciences et des techniques","Méthodes expérimentales, concepts et instrumentation en sciences de la matière et en ingénierie pour le vivant"]
-i=0
+
+sectionsdico={1:"Interactions, particules, noyaux, du laboratoire au cosmos",2:"Théories physiques : méthodes, modèles et applications",3:"Matière condensée : structures et propriétés électroniques",4:"Atomes et molécules, optique et lasers, plasmas chauds",5:"Matière condensée : organisation et dynamique",6:"Sciences de l'information : fondements de l'informatique, calculs, algorithmes, représentations, exploitations",7:"Sciences de l'information : signaux, images, langues, automatique, robotique, interactions, systèmes intégrés matériel-logiciel",8:"Micro- et nanotechnologies, micro- et nanosystèmes, photonique, électronique, électromagnétisme, énergie électrique",9:"Mécanique des solides. Matériaux et structures. Biomécanique. Acoustique",10:"Milieux fluides et réactifs : transports, transferts, procédés de transformation",11:"Systèmes et matériaux supra et macromoléculaires : élaboration, propriétés, fonctions",12:"Architectures moléculaires : synthèses, mécanismes et propriétés",13:"Chimie physique, théorique et analytique",14:"Chimie de coordination, catalyse, interfaces et procédés",15:"Chimie des matériaux, nanomatériaux et procédés",16:"Chimie et vivant",17:"Système solaire et univers lointain",18:"Terre et planètes telluriques : structure, histoire, modèles",19:"Système terre : enveloppes superficielles",20:"Biologie moléculaire et structurale, biochimie",21:"Organisation, expression, évolution des génomes. Bioinformatique et biologie des systèmes",22:"Biologie cellulaire, développement, évolution-développement",23:"Biologie végétale intégrative",24:"Physiologie, vieillissement, tumorigenèse",25:"Neurobiologie moléculaire et cellulaire, neurophysiologie",26:"Cerveau, cognition, comportement",27:"Relations hôte-pathogène, immunologie, inflammation",28:"Pharmacologie-ingénierie et technologies pour la santé-imagerie biomédicale",29:"Biodiversité, évolution et adaptations biologiques : des macromolécules aux communautés",30:"Surface continentale et interfaces",31:"Hommes et milieux : évolution, interactions",32:"Mondes anciens et médiévaux",33:"Mondes modernes et contemporains",34:"Sciences du langage",35:"Sciences philosophiques et philologiques, sciences de l'art",36:"Sociologie et sciences du droit",37:"Economie et gestion",38:"Anthropologie et étude comparative des sociétés contemporaines",39:"Espaces, territoires et sociétés",40:"Politique, Pouvoir, Organisation",41:"Mathématiques et interactions des mathématiques","Interdisciplinaires":"Commissions interdisciplinaires"}
+
+
 #Création de l'arborescence des json
-for section in sections:
-    jsonSec={"key": "section "+str(section),"name": sectionsTitle[i],"values":[]}
-    jsonSecClasse1={"key": "section "+str(section),"name": sectionsTitle[i],"values":[]}
-    jsonSecClasse2={"key": "section "+str(section),"name": sectionsTitle[i],"values":[]}
+for numsection,namesection  in sectionsdico.items():   
+    if numsection == "Interdisciplinaires":
+        key=numsection
+    else:
+        key="Section "+str(numsection)
+    jsonSec={"key": key,"name": namesection,"values":[]}
+    jsonSecClasse1={"key": key,"name": namesection,"values":[]}
+    jsonSecClasse2={"key": key,"name": namesection,"values":[]}
     for arret in arretesCRSorted:
         if arret.classe=="1": 
-            jsonSec["values"].append([arret.year,arret.postes[section]])
-            jsonSecClasse1["values"].append([arret.year,arret.postes[section]])
+            jsonSec["values"].append([arret.year,arret.postes[numsection]])
+            jsonSecClasse1["values"].append([arret.year,arret.postes[numsection]])
         else:           
             classe1=jsonSec["values"][-1][-1]
-            classe2=classe1+arret.postes[section]
+            classe2=classe1+arret.postes[numsection]
             jsonSec["values"][-1]=[arret.year,classe2]
-            jsonSecClasse2["values"].append([arret.year,arret.postes[section]])
+            jsonSecClasse2["values"].append([arret.year,arret.postes[numsection]])
     jsontab.append(jsonSec)
     jsonTabClasse1.append(jsonSecClasse1)
     jsonTabClasse2.append(jsonSecClasse2)
-    i=i+1
 
 out_file = open("postes-CR-CNRS.json","w")
 json.dump(jsontab,out_file, indent=4)     
