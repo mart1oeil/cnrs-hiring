b54086ea5b8d1f4b26d697d604dde81fd75e8144
Martin Clavey <martin.clavey@gmail.com>
2018-12-21 15:24:26 +0100

    ajout des postes de techniciens

--- cnrs_hiring.py
+++ cnrs_hiring.py
@@ -8,7 +8,6 @@ création de poste de chercheurs au CNRS.
 import json
 import sys
 from operator import attrgetter
-#import urllib
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -26,9 +25,9 @@ class Arrete(object):
 	def __str__(self):
 		return  "Arrêté postes CNRS Année %s, classe %s" % (self.year, self.classe)
 
-class ArreteIE(Arrete):
+class ArreteITA(Arrete):
 	'''
-	Un arreteIE est une page web de legifrance créée chaque année
+	Un ArreteITA est une page web de legifrance créée chaque année
 	qui contient le nombre de création de postes d'ingénieurs d'étude CNRS pour chaque section.
 	'''
 	def __init__(self, url):
@@ -96,9 +95,9 @@ class ArreteIE(Arrete):
 			total_bap = 0
 			bap_name = bap_table[0][0]
 			text = bap_table[0][1]
-			postes_table = re.findall(r"[Concours|CONCOURS] [n|N]° [0-9]+</p>[<p>]*[<p align=\"left\">]*[<br/>]*[ ]*[0-9]+ ", text)
+			postes_table = re.findall(r"[Concours|CONCOURS] [n|N]° [0-9]+</p>[<p>]*[<p align=\"left\">]*[<br/>]*[ ]*[0-9]+", text)
 			for postes in postes_table:
-				nbr_postes = re.match(r"[Concours|CONCOURS] [n|N]° [0-9]+</p>[<p>]*[<p align=\"left\">]*[<br/>]*[ ]*([0-9]+) ", postes)
+				nbr_postes = re.match(r"[Concours|CONCOURS] [n|N]° [0-9]+</p>[<p>]*[<p align=\"left\">]*[<br/>]*[ ]*([0-9]+)", postes)
 				total_bap = total_bap+int(nbr_postes.group(1))
 
 
@@ -114,9 +113,9 @@ class ArreteCR(Arrete):
 	def __init__(self, url):
 		Arrete.__init__(self, url)
 		self.postes = ({1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0,
-								 12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0,
-									24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0, 31:0, 32:0, 33:0, 34:0, 35:0,
-									36:0, 37:0, 38:0, 39:0, 40:0, 41:0, "Interdisciplinaires":0, "Total":0})
+                  12:0, 13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0,
+                  24:0, 25:0, 26:0, 27:0, 28:0, 29:0, 30:0, 31:0, 32:0, 33:0, 34:0, 35:0,
+                  36:0, 37:0, 38:0, 39:0, 40:0, 41:0, "Interdisciplinaires":0, "Total":0})
 
 	def attrib_postes(self, num_section, total_section):
 		'''
@@ -127,7 +126,7 @@ class ArreteCR(Arrete):
 		'''
 		# correspondance sections avant 2013 et après 2013
 		corresps = ({1:41, 3:1, 6:3, 7:[6, 7], 20:30, 21:20, 22:21, 23:22, 24:[24, 25, 27],
-							  25:24, 26:22, 27:26, 28:23, 30:28})
+               25:24, 26:22, 27:26, 28:23, 30:28})
 
 		if self.year < 2013 and  (num_section in corresps):
 
@@ -141,11 +140,11 @@ class ArreteCR(Arrete):
 					self.postes[new_section] = self.postes[new_section]+postescorresp
 				#ajout du relicat de la redistribution à la dernière section
 				self.postes[corresps[num_section][-1]] = (self.postes[corresps[num_section][-1]]
-											  + nb_reste)
+                                              + nb_reste)
 			# sinon on ajoute à la section correspondante
 			else:
 				self.postes[corresps[num_section]] = (self.postes[corresps[num_section]]
-										  + total_section)
+                                          + total_section)
 		else:
 			self.postes[num_section] = self.postes[num_section]+total_section
 		self.postes["Total"] = self.postes["Total"] + total_section
@@ -156,7 +155,7 @@ class ArreteCR(Arrete):
 		'''
 		text = section_table[0][1]
 		commissions = (re.split("Commission interdisciplinaire",
-					  							str(section_table[0]), flags=re.S))
+                          str(section_table[0]), flags=re.S))
 		#Si on a des commissions, il faut les intégrer au tableau
 		if len(commissions) > 1:
 			for commission in commissions[1:]:
@@ -175,7 +174,7 @@ class ArreteCR(Arrete):
 					# On attribue à l'indice du tableau correspondant
 					# au numéro de commission le nombre de postes attribués
 					self.postes["Interdisciplinaires"] = (self.postes["Interdisciplinaires"]
-						   														  +total_comm)
+                                           +total_comm)
 			text = commissions[0]
 		return text
 
@@ -235,72 +234,73 @@ class ArreteCR(Arrete):
 		return self.postes
 
 SECTIONS_DICO = ({1:"Interactions, particules, noyaux, du laboratoire au cosmos",
-							  2:"Théories physiques : méthodes, modèles et applications",
-							  3:"Matière condensée : structures et propriétés électroniques",
-							  4:"Atomes et molécules, optique et lasers, plasmas chauds",
-							  5:"Matière condensée : organisation et dynamique",
-							  6:("Sciences de l'information : fondements de l'informatique, calculs, "
-									"algorithmes, représentations, exploitations"),
-							  7:("Sciences de l'information : signaux, images, langues, automatique, "
-									"robotique, interactions, systèmes intégrés matériel-logiciel"),
-							  8:("Micro- et nanotechnologies, micro- et nanosystèmes, photonique, "
-									"électronique, électromagnétisme, énergie électrique"),
-							  9:"Mécanique des solides. Matériaux et structures. Biomécanique. Acoustique",
-							  10:("Milieux fluides et réactifs : transports, "
-								  "transferts, procédés de transformation"),
-							  11:("Systèmes et matériaux supra et macromoléculaires "
-								  ": élaboration, propriétés, fonctions"),
-							  12:"Architectures moléculaires : synthèses, mécanismes et propriétés",
-							  13:"Chimie physique, théorique et analytique",
-							  14:"Chimie de coordination, catalyse, interfaces et procédés",
-							  15:"Chimie des matériaux, nanomatériaux et procédés",
-							  16:"Chimie et vivant",
-							  17:"Système solaire et univers lointain",
-							  18:"Terre et planètes telluriques : structure, histoire, modèles",
-							  19:"Système terre : enveloppes superficielles",
-							  20:"Biologie moléculaire et structurale, biochimie",
-							  21:("Organisation, expression, évolution des génomes. "
-								  "Bioinformatique et biologie des systèmes"),
-							  22:"Biologie cellulaire, développement, évolution-développement",
-							  23:"Biologie végétale intégrative",
-							  24:"Physiologie, vieillissement, tumorigenèse",
-							  25:("Neurobiologie moléculaire et cellulaire, "
-								  "neurophysiologie"),
-							  26:"Cerveau, cognition, comportement",
-							  27:"Relations hôte-pathogène, immunologie, inflammation",
-							  28:("Pharmacologie-ingénierie et technologies "
-								  "pour la santé-imagerie biomédicale"),
-							  29:("Biodiversité, évolution et adaptations biologiques "
-									 ": des macromolécules aux communautés"),
-							  30:"Surface continentale et interfaces",
-							  31:"Hommes et milieux : évolution, interactions",
-							  32:"Mondes anciens et médiévaux",
-							  33:"Mondes modernes et contemporains",
-							  34:"Sciences du langage",
-							  35:"Sciences philosophiques et philologiques, sciences de l'art",
-							  36:"Sociologie et sciences du droit",
-							  37:"Economie et gestion",
-							  38:"Anthropologie et étude comparative des sociétés contemporaines",
-							  39:"Espaces, territoires et sociétés",
-							  40:"Politique, Pouvoir, Organisation",
-							  41:"Mathématiques et interactions des mathématiques",
-							  "Interdisciplinaires":"Commissions interdisciplinaires"})
+                  2:"Théories physiques : méthodes, modèles et applications",
+                  3:"Matière condensée : structures et propriétés électroniques",
+                  4:"Atomes et molécules, optique et lasers, plasmas chauds",
+                  5:"Matière condensée : organisation et dynamique",
+                  6:("Sciences de l'information : fondements de l'informatique, calculs, "
+                     "algorithmes, représentations, exploitations"),
+                  7:("Sciences de l'information : signaux, images, langues, automatique, "
+                     "robotique, interactions, systèmes intégrés matériel-logiciel"),
+                  8:("Micro- et nanotechnologies, micro- et nanosystèmes, photonique, "
+                     "électronique, électromagnétisme, énergie électrique"),
+                  9:"Mécanique des solides. Matériaux et structures. Biomécanique. Acoustique",
+                  10:("Milieux fluides et réactifs : transports, "
+                      "transferts, procédés de transformation"),
+                  11:("Systèmes et matériaux supra et macromoléculaires "
+                      ": élaboration, propriétés, fonctions"),
+                  12:"Architectures moléculaires : synthèses, mécanismes et propriétés",
+                  13:"Chimie physique, théorique et analytique",
+                  14:"Chimie de coordination, catalyse, interfaces et procédés",
+                  15:"Chimie des matériaux, nanomatériaux et procédés",
+                  16:"Chimie et vivant",
+                  17:"Système solaire et univers lointain",
+                  18:"Terre et planètes telluriques : structure, histoire, modèles",
+                  19:"Système terre : enveloppes superficielles",
+                  20:"Biologie moléculaire et structurale, biochimie",
+                  21:("Organisation, expression, évolution des génomes. "
+                      "Bioinformatique et biologie des systèmes"),
+                  22:"Biologie cellulaire, développement, évolution-développement",
+                  23:"Biologie végétale intégrative",
+                  24:"Physiologie, vieillissement, tumorigenèse",
+                  25:("Neurobiologie moléculaire et cellulaire, "
+                      "neurophysiologie"),
+                  26:"Cerveau, cognition, comportement",
+                  27:"Relations hôte-pathogène, immunologie, inflammation",
+                  28:("Pharmacologie-ingénierie et technologies "
+                      "pour la santé-imagerie biomédicale"),
+                  29:("Biodiversité, évolution et adaptations biologiques "
+                      ": des macromolécules aux communautés"),
+                  30:"Surface continentale et interfaces",
+                  31:"Hommes et milieux : évolution, interactions",
+                  32:"Mondes anciens et médiévaux",
+                  33:"Mondes modernes et contemporains",
+                  34:"Sciences du langage",
+                  35:"Sciences philosophiques et philologiques, sciences de l'art",
+                  36:"Sociologie et sciences du droit",
+                  37:"Economie et gestion",
+                  38:"Anthropologie et étude comparative des sociétés contemporaines",
+                  39:"Espaces, territoires et sociétés",
+                  40:"Politique, Pouvoir, Organisation",
+                  41:"Mathématiques et interactions des mathématiques",
+                  "Interdisciplinaires":"Commissions interdisciplinaires"})
 
 
 BAP_DICO = ({"A":"Sciences du vivant, de la terre et de l'environnement",
-				"B":"Sciences chimiques et science des matériaux",
-				"C":"Sciences de l'ingénieur et instrumentation scientifique",
-				"D":"Sciences humaines et sociales",
-				"E":"Informatique, statistiques et calcul scientifique",
-				"F":"Culture, communication, production et diffusion des savoirs)",
-				"G":"Patrimoine immobilier, logistique, restauration et prévention",
-				"H":"Gestion de la recherche",
-				"I":"",
-				"J":"Gestion et pilotage"})
+             "B":"Sciences chimiques et science des matériaux",
+             "C":"Sciences de l'ingénieur et instrumentation scientifique",
+             "D":"Sciences humaines et sociales",
+             "E":"Informatique, statistiques et calcul scientifique",
+             "F":"Culture, communication, production et diffusion des savoirs)",
+             "G":"Patrimoine immobilier, logistique, restauration et prévention",
+             "H":"Gestion de la recherche",
+             "I":"",
+             "J":"Gestion et pilotage"})
 
 
 FILE_LIST_ARRETES_CR = "arretes-cnrs-cr.txt"
 FILE_LIST_ARRETES_IE = "arretes-cnrs-ie.txt"
+FILE_LIST_ARRETES_T = "arretes-cnrs-t.txt"
 
 def liste_arretes_tries(file, mode):
 	'''
@@ -317,13 +317,105 @@ def liste_arretes_tries(file, mode):
 
 		if mode == "cr":
 			arret = ArreteCR(url_arret)
-		if mode == "ie":
-			arret = ArreteIE(url_arret)
+		if mode == "ie" or mode == "t":
+			arret = ArreteITA(url_arret)
 		arret.postes_cnrs()
 		arretes.append(arret)
 		#On trie par année
 		arretes = sorted(arretes, key=attrgetter('year'))
 	return arretes
+
+def build_cr_jsonfile():
+	'''
+	Cette fonction crée les fichiers json pour les catégories de postes
+	de chargés de recherche
+	'''
+	arretes = liste_arretes_tries(FILE_LIST_ARRETES_CR, "cr")
+	json_tab = []
+	json_tab_classe1 = []
+	json_tab_classe2 = []
+	#Création de l'arborescence des json
+	for num_sec, name_section in SECTIONS_DICO.items():
+		if num_sec == "Interdisciplinaires":
+			key = num_sec
+		else:
+			key = "Section "+str(num_sec)
+		json_sec = {"key": key, "name": name_section, "values":[]}
+		json_sec_classe1 = {"key": key, "name": name_section, "values":[]}
+		json_sec_classe2 = {"key": key, "name": name_section, "values":[]}
+		for arret in arretes:
+			if arret.classe == "1":
+				json_sec["values"].append([arret.year, arret.postes[num_sec]])
+				json_sec_classe1["values"].append([arret.year, arret.postes[num_sec]])
+			elif arret.classe == "2":
+				classe1 = json_sec["values"][-1][-1]
+				classe2 = classe1+arret.postes[num_sec]
+				json_sec["values"][-1] = [arret.year, classe2]
+				json_sec_classe2["values"].append([arret.year, arret.postes[num_sec]])
+			elif arret.classe == "classe normale":
+				json_sec["values"].append([arret.year, arret.postes[num_sec]])
+		json_tab.append(json_sec)
+		json_tab_classe1.append(json_sec_classe1)
+		json_tab_classe2.append(json_sec_classe2)
+	with open("postes-CR-CNRS.json", "w") as out_file:
+		json.dump(json_tab, out_file, indent=4)
+	with open("postes-CR-CNRS-Classe1.json", "w")as out_file:
+		json.dump(json_tab_classe1, out_file, indent=4)
+	with open("postes-CR-CNRS-Classe2.json", "w")as out_file:
+		json.dump(json_tab_classe2, out_file, indent=4)
+
+
+def build_ita_jsonfile(mode):
+	'''
+	Cette fonction crée les fichiers json pour les catégories de postes ita
+	(ingénieurs, techniciens, assistants)
+	'''
+	total_file_name = "postes-"+mode.upper()+"-CNRS.json"
+	ext_file_name = "postes-"+mode.upper()+"-CNRS-Externe.json"
+	epr_file_name = "postes-"+mode.upper()+"-CNRS-EPR.json"
+	if mode == "ie":
+		arretes = liste_arretes_tries(FILE_LIST_ARRETES_IE, mode)
+
+	else:
+		arretes = liste_arretes_tries(FILE_LIST_ARRETES_T, mode)
+	json_tab = []
+	json_tab_ext = []
+	json_tab_epr = []
+	for letter_bap, name_bap in BAP_DICO.items():
+		key = "BAP "+str(letter_bap)
+		json_bap = {"key": key, "name": name_bap, "values":[]}
+		json_bap_ext = {"key": key, "name": name_bap, "values":[]}
+		json_bap_epr = {"key": key, "name": name_bap, "values":[]}
+		for arret in arretes:
+
+			if json_bap["values"] and arret.year == json_bap["values"][-1][0]:
+				json_bap["values"].append([arret.year, json_bap["values"].pop()[1]+arret.postes[letter_bap]])
+			else:
+				json_bap["values"].append([arret.year, arret.postes[letter_bap]])
+			if arret.epr:
+				if json_bap_epr["values"] and arret.year == json_bap_epr["values"][-1][0]:
+					json_bap_epr["values"].append([arret.year,
+									json_bap_epr["values"].pop()[1]+
+									arret.postes[letter_bap]])
+				else:
+					json_bap_epr["values"].append([arret.year, arret.postes[letter_bap]])
+			else:
+				if json_bap_ext["values"] and arret.year == json_bap_ext["values"][-1][0]:
+					json_bap_ext["values"].append([arret.year,
+									json_bap_ext["values"].pop()[1]
+									+arret.postes[letter_bap]])
+				else:
+					json_bap_ext["values"].append([arret.year, arret.postes[letter_bap]])
+
+		json_tab_ext.append(json_bap_ext)
+		json_tab_epr.append(json_bap_epr)
+		json_tab.append(json_bap)
+	with open(total_file_name, "w") as out_file:
+		json.dump(json_tab, out_file, indent=4)
+	with open(ext_file_name, "w") as out_file:
+		json.dump(json_tab_ext, out_file, indent=4)
+	with open(epr_file_name, "w") as out_file:
+		json.dump(json_tab_epr, out_file, indent=4)
 
 def main(argv):
 	'''
@@ -335,82 +427,10 @@ def main(argv):
 	'''
 
 	mode = argv[0]
-
-
-
 	if mode == "cr":
-
-		arretes = liste_arretes_tries(FILE_LIST_ARRETES_CR, mode)
-		json_tab = []
-		json_tab_classe1 = []
-		json_tab_classe2 = []
-		#Création de l'arborescence des json
-		for num_sec, name_section in SECTIONS_DICO.items():
-			if num_sec == "Interdisciplinaires":
-				key = num_sec
-			else:
-				key = "Section "+str(num_sec)
-			json_sec = {"key": key, "name": name_section, "values":[]}
-			json_sec_classe1 = {"key": key, "name": name_section, "values":[]}
-			json_sec_classe2 = {"key": key, "name": name_section, "values":[]}
-			for arret in arretes:
-				if arret.classe == "1":
-					json_sec["values"].append([arret.year, arret.postes[num_sec]])
-					json_sec_classe1["values"].append([arret.year, arret.postes[num_sec]])
-				elif arret.classe == "2":
-					classe1 = json_sec["values"][-1][-1]
-					classe2 = classe1+arret.postes[num_sec]
-					json_sec["values"][-1] = [arret.year, classe2]
-					json_sec_classe2["values"].append([arret.year, arret.postes[num_sec]])
-				elif arret.classe == "classe normale":
-					json_sec["values"].append([arret.year, arret.postes[num_sec]])
-			json_tab.append(json_sec)
-			json_tab_classe1.append(json_sec_classe1)
-			json_tab_classe2.append(json_sec_classe2)
-		with open("postes-CR-CNRS.json", "w") as out_file:
-			json.dump(json_tab, out_file, indent=4)
-		with open("postes-CR-CNRS-Classe1.json", "w")as out_file:
-			json.dump(json_tab_classe1, out_file, indent=4)
-		with open("postes-CR-CNRS-Classe2.json", "w")as out_file:
-			json.dump(json_tab_classe2, out_file, indent=4)
-
-	elif mode == "ie":
-		arretes = liste_arretes_tries(FILE_LIST_ARRETES_IE, mode)
-		json_tab = []
-
-		json_tab_ext = []
-		json_tab_epr = []
-		for letter_bap, name_bap in BAP_DICO.items():
-			key = "BAP "+str(letter_bap)
-			json_bap = {"key": key, "name": name_bap, "values":[]}
-			json_bap_ext = {"key": key, "name": name_bap, "values":[]}
-			json_bap_epr = {"key": key, "name": name_bap, "values":[]}
-			for arret in arretes:
-
-				if json_bap["values"] and arret.year == json_bap["values"][-1][0]:
-					json_bap["values"].append([arret.year, json_bap["values"].pop()[1]+arret.postes[letter_bap]])
-				else:
-					json_bap["values"].append([arret.year, arret.postes[letter_bap]])
-				if arret.epr:
-					if json_bap_epr["values"] and arret.year == json_bap_epr["values"][-1][0]:
-						json_bap_epr["values"].append([arret.year, json_bap_epr["values"].pop()[1]+arret.postes[letter_bap]])
-					else:
-						json_bap_epr["values"].append([arret.year, arret.postes[letter_bap]])
-				else:
-					if json_bap_ext["values"] and arret.year == json_bap_ext["values"][-1][0]:
-						json_bap_ext["values"].append([arret.year, json_bap_ext["values"].pop()[1]+arret.postes[letter_bap]])
-					else:
-						json_bap_ext["values"].append([arret.year, arret.postes[letter_bap]])
-						
-			json_tab_ext.append(json_bap_ext)
-			json_tab_epr.append(json_bap_epr)
-			json_tab.append(json_bap)
-		with open("postes-IE-CNRS.json", "w") as out_file:
-			json.dump(json_tab, out_file, indent=4)	
-		with open("postes-IE-CNRS-Externe.json", "w") as out_file:
-			json.dump(json_tab_ext, out_file, indent=4)	
-		with open("postes-IE-CNRS-EPR.json", "w") as out_file:
-			json.dump(json_tab_epr, out_file, indent=4)	
+		build_cr_jsonfile()
+	elif mode == "ie" or mode == "t":
+		build_ita_jsonfile(mode)
 	else:
 		print("Pas de catégorie de postes de ce type")
 
